3a900ff96d165aec8a1a6152330740ff54e8fcb7
Claudio Senatore <senatore@fs-net.de>
2023-09-28 16:56:34 +0200

    implement sshkeyboard in main programm
    sshkeyboard is used to read keyboard input via stdio. This allows to
    recognize keystrokes via TeraTerm or SSH without having to connect a
    keyboard to the target device.

--- opencv/src/THT-Classificator.py
+++ opencv/src/THT-Classificator.py
@@ -1,4 +1,5 @@
 import os
+import threading
 import logging
 import numpy as np
 import cv2
@@ -6,6 +7,32 @@ import time
 import argparse
 from SettingsFile import xmlSettings
 from FrameProcessing import FrameProccessing
+from sshkeyboard import listen_keyboard
+from dataclasses import dataclass
+
+@dataclass
+class Keyboard:
+    f5: bool
+    f6: bool
+    f7: bool
+
+key_pressed = Keyboard(False, False, False)
+
+def press(key):
+    if key=='f5':
+        key_pressed.f5 = True
+    elif key=='f6':
+        key_pressed.f6 = True
+    elif key=='f7':
+        key_pressed.f7 = True
+
+def listen_keyboard_wrapper():
+    listen_keyboard(
+        on_press=press,
+        delay_second_char=2,
+        delay_other_chars=0.1,
+    )
+
 
 def main():
 
@@ -33,12 +60,19 @@ def main():
     Settings= xmlSettings(args.settings)
     img_processing = FrameProccessing(Settings)
 
+    #Set Keyboard polling
+    if logging.getLogger().getEffectiveLevel() == logging.DEBUG:
+        thread = threading.Thread(target=listen_keyboard_wrapper)
+        thread.start()
+
     #MAIN loop
     while True:
         img_processing.update()
+
+        #Show Captured Frame
         img_processing.wrt_frame = img_processing.cap_frame
 
-        #Draw Rectangle
+        #Draw Rectangle around object
         if hasattr(img_processing, 'scaled_box'):
             x, y, w, h = cv2.boundingRect(img_processing.scaled_box)
             cv2.drawContours(img_processing.wrt_frame, [img_processing.scaled_box], 0, (255,255,0),2)
@@ -46,6 +80,7 @@ def main():
 
         #when debug is enabled
         if logging.getLogger().getEffectiveLevel() == logging.DEBUG:
+
             #Show Calibration Frame
             if hasattr(img_processing, 'calibrate_frame'):
                 try:
@@ -53,6 +88,7 @@ def main():
                     img_processing.wrt_frame[0:shape1[0], 0:shape1[1], :] = img_processing.calibrate_frame
                 except:
                     logging.error("Could not display calibrate_frame")
+
             #Show rotated object
             if hasattr(img_processing, 'object_img'):
                 try:
@@ -60,7 +96,13 @@ def main():
                     img_processing.wrt_frame[0:shape2[0], 1920-shape2[1]:, :] = img_processing.object_img[:,:,:]
                 except:
                     logging.error("Could not display object_img")
+                if key_pressed.f5:
+                    cv2.imwrite("test.png", img_processing.object_img)
+                    print('saved image as test.png')
+                    key_pressed.f5 = False
+
             img_processing.Settings.parse(args.settings)
+
 
 if __name__ == '__main__':
     main()
